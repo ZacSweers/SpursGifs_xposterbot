7619715b0ac389ad2ddcbcc2b46645f05a60cbeb
Henri Sweers <pandanomic@gmail.com>
2014-01-19 17:12:34 -0600

    Trying to understand memcache

--- spursgifsbot/bot.py
+++ spursgifsbot/bot.py
@@ -88,6 +88,7 @@ def bot():
         if validate_submission(submission):
             if running_on_heroku:
                 mc.set(str(submission.id), "True")
+                assert mc.get(str(submission.id)) == "True"
             else:
                 already_done.append(submission.id)
             print "(New Post)"
@@ -126,6 +127,7 @@ def submit(subreddit, submission):
         if gfy_converted:
             if running_on_heroku:
                 mc.set(str(new_submission.id), "True")
+                assert mc.get(str(new_submission.id)) == "True"
             else:
                 already_done.append(new_submission.id)
 
@@ -152,7 +154,7 @@ def validate_submission(submission):
     # check domain and extension validity
     if submission.domain in allowedDomains or extension(submission.url) in allowedExtensions:
         # Running on heroku, check the memcache
-        if running_on_heroku and not mc.get(str(submission.id)):
+        if running_on_heroku and not mc.get(str(submission.id)) and mc.get(str(submission.id)) != "True":
             return True
         if submission.id not in already_done:
             return True
